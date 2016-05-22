f9ac287744e50851618dcce17f2c5635f5bf099f
jeremylindsayni <jeremy.lindsay@outlook.com>
2016-05-22 17:12:09 +0100

    Initial commit of camera implementation.

---
+++ CameraDevice.cs
@@ -0,0 +1,48 @@
+﻿using Magellanic.Camera.Interfaces;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
+using Windows.Media.Capture;
+
+namespace Magellanic.Camera
+{
+    public class CameraDevice : ICameraDevice
+    {
+        public MediaCapture ViewFinder { get; set; }
+
+        public void Dispose()
+        {
+            ViewFinder?.Dispose();
+            ViewFinder = null;
+        }
+
+        public async Task<DeviceInformation> GetCameraAtPanelLocation(Panel cameraPosition)
+        {
+            var cameraDevices = await GetCameraDevices();
+
+            return cameraDevices.FirstOrDefault(c => c.EnclosureLocation?.Panel == cameraPosition);
+        }
+
+        public async Task<DeviceInformation> GetDefaultCamera()
+        {
+            var cameraDevices = await GetCameraDevices();
+
+            return cameraDevices.FirstOrDefault();
+        }
+
+        public async Task InitialiseCameraAsync(DeviceInformation cameraToInitialise)
+        {
+            await ViewFinder?.InitializeAsync(
+                new MediaCaptureInitializationSettings
+                {
+                    VideoDeviceId = cameraToInitialise.Id
+                });
+        }
+
+        private async Task<DeviceInformationCollection> GetCameraDevices()
+        {
+            return await DeviceInformation.FindAllAsync(DeviceClass.VideoCapture);
+        }
+    }
+}
